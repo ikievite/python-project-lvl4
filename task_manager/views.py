78c0c566380fae9c69479266ea1bd65a5e7755a3
Alex <ikievite@gmail.com>
2021-07-13 18:02:17 +0300

    added i18n support, updated view

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.utils.translation import gettext as _
 
 
 def index(request):
     return render(request, 'index.html', context={
-        'who': 'World',
+        'greeting': _('Hello, World'),
     })
