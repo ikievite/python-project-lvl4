ccfb117a8dea789b90532842acfabfc7913b1a2c
Alex <ikievite@gmail.com>
2021-07-15 10:08:24 +0300

    updated project urls

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from task_manager import views
+from django.urls import path, include
 
 
 urlpatterns = [
-    path('', views.index),
+    path('', include('tasks.urls')),
     #path('admin/', admin.site.urls),
 ]
