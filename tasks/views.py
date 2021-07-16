35d53c75b3ab4586ed3f9c79a1b5fc612d5606cf
Alex <ikievite@gmail.com>
2021-07-16 20:42:19 +0300

    add log msg to view

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,13 @@
+import logging
 from django.shortcuts import render
 from django.utils.translation import gettext as _
 
 
+logger = logging.getLogger(__name__)
+
+
 def index(request):
+    logger.error('log msg')
     return render(request, 'tasks/index.html', context={
         'greeting': _('Hello, World'),
     })
