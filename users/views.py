33f037e514f280567cd9642be41e6c1f3f13ecc1
Alex <ikievite@gmail.com>
2021-07-20 11:01:45 +0300

    added users list

--- users/views.py
+++ users/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
 
 
 def users(request):
-    return render(request, 'users/users.html', {'line': 'Users'})
+    User = get_user_model()
+    users = User.objects.all()
+    return render(request, 'users/users.html', {'users': users})
