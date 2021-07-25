8b9078318c714d82e3f5e111514053582a1edf2b
ikievite <ikievite@gmail.com>
2021-07-25 10:03:23 +0300

    added a bottstrap msg, that informs that the `account has been created`

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
+from django.contrib import messages
 from .forms import UserCreateForm
 
 
@@ -14,6 +15,7 @@ def create(request):
         form = UserCreateForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, f'Your account has been created! You are now able to log in')
             return redirect('login')
     else:
         form = UserCreateForm()
