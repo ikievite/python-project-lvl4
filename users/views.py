9afe21434814cd7e0bce57c3c9bb7a7ba3333b36
ikievite <ikievite@gmail.com>
2021-07-25 10:21:49 +0300

    updated i18n translation

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
 from django.contrib import messages
+from django.utils.translation import gettext as _
 from .forms import UserCreateForm
 
 
@@ -15,7 +16,7 @@ def create(request):
         form = UserCreateForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, f'Your account has been created! You are now able to log in')
+            messages.success(request, _(f'Your account has been created! You are now able to log in'))
             return redirect('login')
     else:
         form = UserCreateForm()
