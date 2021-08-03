a1864f175858b6b595ec0e5906d3bbcfcdc2a952
ikievite <ikievite@gmail.com>
2021-08-03 10:32:52 +0300

    added simple test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class UsersViewTests(TestCase):
+    def test_is_url_exists(self):
+        response = self.client.get(reverse('users'))
+        self.assertEqual(response.status_code, 200)
