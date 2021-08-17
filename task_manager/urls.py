c63075815f7dea70cb6fef55d3c806ff8bbbfbc8
ikievite <ikievite@gmail.com>
2021-08-17 10:17:12 +0300

    linter`s improvements

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -15,13 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path, include
-
+from django.urls import include, path
 
 urlpatterns = [
     path('', include('tasks.urls')),
     path('users/', include('users.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path(
+        'logout/',
+        auth_views.LogoutView.as_view(template_name='users/logout.html'),
+        name='logout',
+        ),
     path('admin/', admin.site.urls),
 ]
