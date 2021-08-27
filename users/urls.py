df656d7d532f516d27fc31b7e15eba39f2e81d2e
ikievite <ikievite@gmail.com>
2021-08-27 09:34:30 +0300

    updated the url that leads to user creation page

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from . import views
-from .views import UserDeleteView, UserListView, UserUpdateView
+from .views import UserDeleteView, UserListView, UserUpdateView, UserCreateView
 
 urlpatterns = [
     path('', UserListView.as_view(), name='users'),
-    path('create/', views.create, name='create-user'),
+    path('create/', UserCreateView.as_view(), name='create-user'),
     path('<int:pk>/update/', UserUpdateView.as_view(), name='update-user'),
     path('<int:pk>/delete/', UserDeleteView.as_view(), name='delete-user'),
 ]
