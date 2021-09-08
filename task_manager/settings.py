8d6de8efbe7e8b477fa8431120b30cfe943da908
ikievite <ikievite@gmail.com>
2021-09-08 10:14:13 +0300

    removed coments and updated DATABASES vars

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -117,15 +117,8 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
+    'default': dj_database_url.config(),
 }
-
-DATABASES['default'] = dj_database_url.config(
-    conn_max_age=DB_CONN_MAX_AGE,
-)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
