06e56ae1b35ed85da186b56fcf5d322fb09e4aa0
ikievite <ikievite@gmail.com>
2021-09-08 09:58:30 +0300

    added `dotenv` import

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -13,10 +13,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
 import dj_database_url
 
 DB_CONN_MAX_AGE = 600
 
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
