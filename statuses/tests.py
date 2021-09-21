5ae3ecad01f5c44853a89b4d4ad911c3f4f261e8
ikievite <ikievite@gmail.com>
2021-09-21 13:33:32 +0300

    added tests for `StatusListView`

--- statuses/tests.py
+++ statuses/tests.py
@@ -1,5 +1,31 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
 
 
-class StatusCreateView(TestCase):
-    pass
+class StatusListViewTest(TestCase):
+    def setUp(self):
+        test_user = User.objects.create_user(
+            username='testuser',
+            password='superpass',
+        )
+        test_user.save()
+
+    def test_redirect_if_not_logged(self):
+        response = self.client.get(reverse('statuses'))
+        self.assertRedirects(response, '/login/')
+
+    def test_logged_in(self):
+        login = self.client.login(username='testuser', password='superpass')
+        response = self.client.get(reverse('statuses'))
+        self.assertEqual(str(response.context['user']), 'testuser')
+
+    def test_show_statuses(self):
+        login = self.client.login(username='testuser', password='superpass')
+        response = self.client.get(reverse('statuses'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_uses_correct_template(self):
+        login = self.client.login(username='testuser', password='superpass')
+        response = self.client.get(reverse('statuses'))
+        self.assertTemplateUsed(response, 'statuses/statuses.html')
