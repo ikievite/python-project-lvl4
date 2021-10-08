c3957d2e076a128b2f5db1021885fa9a416f40a3
ikievite <ikievite@gmail.com>
2021-10-08 10:28:51 +0300

    added `TaskListView` to `tasks` app

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,12 @@
 import logging
-from django.shortcuts import render
-from django.utils.translation import gettext as _
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import redirect, render
+from django.utils.translation import gettext as _
+from django.views.generic import ListView
+
+from .models import Task
 
 logger = logging.getLogger(__name__)
 
@@ -11,3 +16,14 @@ def index(request):
     return render(request, 'tasks/index.html', context={
         'greeting': _('Hello, World'),
     })
+
+
+class TaskListView(LoginRequiredMixin, ListView):
+    model = Task
+    template_name = 'tasks/tasks.html'
+    context_object_name = 'tasks'
+    need_loging_message = _('You are not authorized! Please sign in.')
+
+    def handle_no_permission(self):
+        messages.error(self.request, self.need_loging_message)
+        return redirect('login')
