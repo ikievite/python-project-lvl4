eabbe8855c4c6ae8cc07b24c7184dc2c4edf8926
ikievite <ikievite@gmail.com>
2021-10-08 10:28:09 +0300

    added urls for `tasks` app

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views
 
+from . import views
+from .views import TaskListView
 
 urlpatterns = [
     path('', views.index, name='tasks-home'),
+    path('tasks/', TaskListView.as_view(), name='tasks'),
 ]
