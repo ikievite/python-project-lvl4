1890e554fdb481079eeaf9bed24f11992c00dfc5
ikievite <ikievite@gmail.com>
2021-10-13 15:30:08 +0300

    added `TaskCreateView`

--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,14 @@
 import logging
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
-from django.views.generic import ListView
+from django.views.generic import CreateView, ListView
 
+from .forms import TaskCreateForm
 from .models import Task
 
 logger = logging.getLogger(__name__)
@@ -27,3 +30,24 @@ class TaskListView(LoginRequiredMixin, ListView):
     def handle_no_permission(self):
         messages.error(self.request, self.need_loging_message)
         return redirect('login')
+
+
+class TaskCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
+    model = Task
+    template_name = 'tasks/create.html'
+    success_url = reverse_lazy('tasks')
+    missed_rights_message = _('You do not have permission to modify task.')
+    need_loging_message = _('You are not authorized! Please sign in.')
+    success_message = _('Task successfully created')
+    form_class = TaskCreateForm
+
+    def handle_no_permission(self):
+        if self.request.user.is_authenticated:
+            messages.error(self.request, self.missed_rights_message)
+            return redirect('tasks')
+        messages.error(self.request, self.need_loging_message)
+        return redirect('login')
+
+    def form_valid(self, form):
+        form.instance.creator = User.objects.get(id=self.request.user.id)
+        return super().form_valid(form)
